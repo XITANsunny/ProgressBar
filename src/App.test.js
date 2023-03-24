c589fabca1c57245ffc4f3b5184091df80a075ad
XITANsunny <tanxi1991sunny@gmail.com>
2023-03-24 11:12:07 +1100

    wirte test case

--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,10 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+
+test('renders bars demo', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Progress Bars Demo/i);
+  expect(linkElement).toBeInTheDocument();
+});
