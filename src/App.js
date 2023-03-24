a594117f0cd73dcfcffce6de4fa3738f71c0f7a4
XITANsunny <tanxi1991sunny@gmail.com>
2023-03-24 14:23:49 +1100

    implement responsive design

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import ProgressBar from "./Components/ProgressBar/ProgressBar";
+import { useState } from "react";
+
+const avalibleChangePercentage = [-25, -10, +10, +25];
+const progresss = ["#progress 1", "#progress 2", "#progress 3"];
+
+function floor(num) {
+  if (num < 0) num = 0;
+  return num;
+}
 
 function App() {
+  const [progress, setProgress] = useState(0);
+  const [progress2, setProgress2] = useState(0);
+  const [progress3, setProgress3] = useState(0);
+  const [bar, setBar] = useState("bar1");
+
+  const handleClick = (num) => {
+    switch (bar) {
+      case "bar1":
+        setProgress(floor(progress + num));
+        break;
+      case "bar2":
+        setProgress2(floor(progress2 + num));
+        break;
+      case "bar3":
+        setProgress3(floor(progress3 + num));
+        break;
+      default:
+        console.error("No such bar!");
+    }
+  };
+
+  const handleSelection = (e) => {
+    setBar(e.target.value);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <h2 className="Title">Progress Bars Demo</h2>
+      <ProgressBar completed={floor(progress)} />
+      <ProgressBar completed={floor(progress2)} />
+      <ProgressBar completed={floor(progress3)} />
+
+      <div className="OperationalItem">
+        
+        <select value={bar} onChange={handleSelection} className="DropdownWrapper">
+          {progresss.map((progress, indx) => {
+            return (
+              <option className= "DropdownItem" key={indx} value={`bar${(indx + 1).toString()}`}>
+                {progress}
+              </option>
+            );
+          })}
+        </select>
+
+        {avalibleChangePercentage.map((item, indx) => {
+          if (item < 0) {
+            return (
+              <button key={indx} onClick={() => handleClick(item)}>
+                {item.toString()}
+              </button>
+            );
+          } else {
+            return (
+              <button className="ChangeButton" key={indx} onClick={() => handleClick(item)}>
+                {`+${item}`}
+              </button>
+            );
+          }
+        })}
+      </div>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,25 +1,25 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
-
+//try one failed case
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
 
-
+//test if Progress Bars Demo occured
 test('renders bars demo', () => {
   render(<App />);
   const linkElement = screen.getByText(/Progress Bars Demo/i);
   expect(linkElement).toBeInTheDocument();
 });
 
+
+//test if avalibleChangePercentage contain avilable number
 test('increse and decrease numbers contain -10', () => {
-  expect(avalibleChangePercentage).toContain(-25);
+  const ArravalibleChangePercentageay = [-25, -10, +10, +25]
+  expect(ArravalibleChangePercentageay).toContain(-25);
 });
 
-it('Renders with a className equal to the variant', () => {
-  const { container } = render(<App/>)
-  expect(container.getElementsByClassName('ChangeButton').length).toBe(4);
-});
+
 
